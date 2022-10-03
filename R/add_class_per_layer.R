ff01560796dc654f922e06c052016404b4e44416
inofechm <inofechmozes@oicr.on.ca>
2022-10-03 09:52:48 -0400

    fixed bug

--- R/add_class_per_layer.R
+++ R/add_class_per_layer.R
@@ -20,78 +20,99 @@ add_class_per_layer <- function(layer_predictions, layer){
                                         "Sarcoma")
     blood_classes <- c("B cell","CD4+ T cell", "CD8+ T cell", "HSPC", "Macrophage","cDC", "pDC", "ASDC","Plasmablast",
                        "Natural killer cell")
-    cancer_normal_stromal_score <- rowSums(layer_predictions[,cancer_normal_stromal_classes])
-    blood_score <- rowSums(layer_predictions[,blood_classes])
+    cancer_normal_stromal_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% cancer_normal_stromal_classes)]))
+    blood_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% blood_classes)]))
+
     layer_classes <- cbind(layer_predictions, cancer_normal_stromal_score, blood_score)
   }
   if(layer == "layer_2_blood"){
-    B_cell_score <- rowSums(layer_predictions[,c("B cell", "Plasmablast")])
-    non_B_lymphocyte_score <- rowSums(layer_predictions[,c("CD4+ T cell","CD8+ T cell", "Natural killer cell")])
-    macrophage_DC_score <- rowSums(layer_predictions[,c("Macrophage","cDC", "pDC", "ASDC")])
-    HSPC_score <- layer_predictions[,"HSPC"]
+
+    B_cell_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("B cell", "Plasmablast"))]))
+    non_B_lymphocyte_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in%c("CD4+ T cell","CD8+ T cell", "Natural killer cell"))]))
+    macrophage_DC_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in%c("ASDC","Macrophage","cDC", "pDC", "Monocyte"))]))
+    HSPC_score <- layer_predictions[,which(colnames(layer_predictions) %in% "HSPC")]
+
     layer_classes <- cbind(layer_predictions, B_cell_score, non_B_lymphocyte_score, macrophage_DC_score,
                            HSPC_score)
   }
   if(layer == "layer_3_TNK"){
-    CD4_CD8_score <- rowSums(layer_predictions[,c("CD4+ T cell", "CD8+ T cell")])
-    NK_CD8_score <- rowSums(layer_predictions[,c("Natural killer cell", "CD8+ T cell")])
+    CD4_CD8_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in%c("CD4+ T cell", "CD8+ T cell"))]))
+    NK_CD8_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in%c("Natural killer cell", "CD8+ T cell"))]))
+
     layer_classes <- cbind(layer_predictions, CD4_CD8_score, NK_CD8_score)
   }
   if(layer == "layer_3_MDC"){
     macrophage_score <- layer_predictions[,"Macrophage"]
-    DC_score <- rowSums(layer_predictions[,c("ASDC", "cDC", "pDC")])
+    DC_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in%c( "ASDC","cDC", "pDC"))]))
     layer_classes <- cbind(layer_predictions, macrophage_score, DC_score)
   }
   if(layer == "layer_3_BCell"){
-    B_cell_score <- layer_predictions[,"B cell"]
-    plasmablast_score <- layer_predictions[,"Plasmablast"]
+    B_cell_score <- layer_predictions[,which(colnames(layer_predictions) == "B cell")]
+    plasmablast_score <- layer_predictions[,which(colnames(layer_predictions) == "Plasmablast")]
     layer_classes <- cbind(layer_predictions, B_cell_score, plasmablast_score)
   }
   if(layer == "layer_2_non_blood"){
-    stromal_score <- layer_predictions$`Endothelial Cells` +
-      layer_predictions$Fibroblasts + layer_predictions$Myofibroblasts + layer_predictions$`Smooth Muscle Cells`
-    non_stromal_score <- layer_predictions$`Bone Cancer`  +
-      layer_predictions$Sarcoma +  layer_predictions$`Brain Cancer` +
-      layer_predictions$Neuroblastoma + layer_predictions$`Skin Cancer` + layer_predictions$`Bile Duct Cancer` + layer_predictions$`Bladder Cancer` + layer_predictions$`Breast Cancer` +
-      layer_predictions$`Colon/Colorectal Cancer` + layer_predictions$`Endometrial/Uterine Cancer` + layer_predictions$`Esophageal Cancer` +
-      layer_predictions$`Gallbladder Cancer` + layer_predictions$`Gastric Cancer` + layer_predictions$`Kidney Cancer` + layer_predictions$`Liver Cancer` + layer_predictions$`Lung Cancer` +
-      layer_predictions$`Ovarian Cancer` + layer_predictions$`Pancreatic Cancer` + layer_predictions$`Prostate Cancer`
+    stromal_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Endothelial Cells",
+                                                                                                       "Fibroblasts", "Myofibroblasts", "Smooth Muscle Cells",
+                                                                                                       "Cancer Associated Fibroblasts", "Cancer Associated Myofibroblasts"))]))
+    non_stromal_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Bone Cancer",
+                                                                                                           "Sarcoma","Brain Cancer",
+                                                                                                           "Neuroblastoma", "Skin Cancer","Bile Duct Cancer", "Bladder Cancer", "Breast Cancer",
+                                                                                                           "Colon/Colorectal Cancer", "Endometrial/Uterine Cancer", "Esophageal Cancer",
+                                                                                                           "Gallbladder Cancer", "Gastric Cancer",  "Kidney Cancer", "Liver Cancer",  "Lung Cancer",
+                                                                                                           "Ovarian Cancer", "Pancreatic Cancer", "Prostate Cancer"))]))
     layer_classes <- cbind(layer_predictions,stromal_score,non_stromal_score)
   }
   if(layer == "layer_3_non_stromal"){
-    non_GI_score <- layer_predictions$`Endometrial/Uterine Cancer` + layer_predictions$`Ovarian Cancer` + layer_predictions$`Breast Cancer` +
-      layer_predictions$`Lung Cancer` +layer_predictions$`Prostate Cancer` + layer_predictions$`Kidney Cancer`
-    GI_score <- layer_predictions$`Bile Duct Cancer` + layer_predictions$`Bladder Cancer` + layer_predictions$`Colon/Colorectal Cancer` +
-      layer_predictions$`Esophageal Cancer` + layer_predictions$`Gallbladder Cancer` + layer_predictions$`Gastric Cancer` +
-      layer_predictions$`Liver Cancer` + layer_predictions$`Pancreatic Cancer`
-    soft_tissue_neuro_score <- layer_predictions$`Bone Cancer` +
-      layer_predictions$Sarcoma +  layer_predictions$`Brain Cancer` +
-      layer_predictions$Neuroblastoma + layer_predictions$`Skin Cancer` + layer_predictions$`Lung Cancer`
+    non_GI_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Breast Cancer",
+                                                                                                      "Endometrial/Uterine Cancer", "Kidney Cancer",   "Lung Cancer",
+                                                                                                      "Ovarian Cancer", "Prostate Cancer"))]))
+
+    GI_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Bile Duct Cancer", "Bladder Cancer",
+                                                                                                  "Colon/Colorectal Cancer", "Esophageal Cancer",
+                                                                                                  "Gallbladder Cancer", "Gastric Cancer", "Liver Cancer",
+                                                                                                  "Pancreatic Cancer"))]))
+
+    soft_tissue_neuro_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Bone Cancer",
+                                                                                                                 "Sarcoma","Brain Cancer",
+                                                                                                                 "Neuroblastoma", "Skin Cancer", "Lung Cancer"))]))
     layer_classes <- cbind(layer_predictions,soft_tissue_neuro_score, non_GI_score, GI_score)
   }
   if(layer == "layer_4_non_GI"){
-    ov_endo_kid_score <- layer_predictions$`Endometrial/Uterine Cancer` + layer_predictions$`Ovarian Cancer`  + layer_predictions$`Kidney Cancer`
-    breast_lung_prostate_score <- layer_predictions$`Breast Cancer` + layer_predictions$`Lung Cancer` +layer_predictions$`Prostate Cancer`
+    ov_endo_kid_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Endometrial/Uterine Cancer", "Kidney Cancer",
+                                                                                                           "Ovarian Cancer"))]))
+    breast_lung_prostate_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Breast Cancer",
+                                                                                                                    "Lung Cancer", "Prostate Cancer"))]))
+
     layer_classes <- cbind(layer_predictions, ov_endo_kid_score, breast_lung_prostate_score)
   }
   if(layer == "layer_4_GI"){
-    digestive_score <- layer_predictions$`Colon/Colorectal Cancer` + layer_predictions$`Esophageal Cancer` + layer_predictions$`Gastric Cancer`
-    billiary_score <- layer_predictions$`Bile Duct Cancer` + layer_predictions$`Bladder Cancer` + layer_predictions$`Gallbladder Cancer` +
-      layer_predictions$`Liver Cancer` + layer_predictions$`Pancreatic Cancer`
+    digestive_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c(
+      "Colon/Colorectal Cancer", "Esophageal Cancer",
+      "Gastric Cancer"))]))
+
+    billiary_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Bile Duct Cancer", "Bladder Cancer",
+                                                                                                        "Gallbladder Cancer", "Liver Cancer",
+                                                                                                        "Pancreatic Cancer"))]))
+
     layer_classes <- cbind(layer_predictions, billiary_score, digestive_score)
   }
   if(layer == "layer_4_soft_tissue_neuro"){
-    soft_tissue_neuro_score <- layer_predictions$`Brain Cancer` + layer_predictions$Neuroblastoma +
-      layer_predictions$`Bone Cancer`  + layer_predictions$Sarcoma
+    soft_tissue_neuro_score <-rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Bone Cancer",
+                                                                                                                "Sarcoma","Brain Cancer",
+                                                                                                                "Neuroblastoma"))]))
     layer_classes <- cbind(layer_predictions, soft_tissue_neuro_score)
   }
   if(layer == "layer_5_soft_tissue_neuro"){
-    brain_nbm_score <- layer_predictions$`Brain Cancer` + layer_predictions$Neuroblastoma
-    soft_tissue_score <-   layer_predictions$`Bone Cancer`  + layer_predictions$Sarcoma
+    brain_nbm_score <- rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Brain Cancer",
+                                                                                                         "Neuroblastoma"))]))
+    soft_tissue_score <-   rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c("Bone Cancer",
+                                                                                                             "Sarcoma"))]))
     layer_classes <- cbind(layer_predictions, brain_nbm_score, soft_tissue_score)
   }
   if(layer == "layer_3_stromal"){
-    fibroblasts_score <-   layer_predictions$Fibroblasts + layer_predictions$Myofibroblasts
+    fibroblasts_score <-   rowSums(as.data.frame(layer_predictions[,which(colnames(layer_predictions) %in% c(
+      "Fibroblasts", "Myofibroblasts"))]))
     layer_classes <- cbind(layer_predictions, fibroblasts_score)
   }
   if(layer %in% c("layer_4_dendritic","layer_5_biliary",
